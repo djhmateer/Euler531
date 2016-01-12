8f2a327cf700229c15421097672755ee268395be
Ben Psarris <benjamin.psarris@qnrl.com>
2016-01-12 10:51:04 +0000

    update 2

--- p_531/Program.cs
+++ p_531/Program.cs
@@ -5,30 +5,76 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Numerics;
 
+
 namespace p_531
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(totientOf(102));
-
-
+            DateTime start = DateTime.Now;
+            long sumOfSolution = 0;
+            List<TotientPair> listOfTotients = new List<TotientPair>();
+            for (long i = 1000000; i < 1005000; i++)
+            {
+                listOfTotients.Add(new TotientPair(i));
+            }
+            for (int n = 0; n < 4999; n++)
+            {
+                for (int m = n+1; m < 5000; m++)
+                {
+                    if (listOfTotients[n].Totient % listOfTotients[n].Number == listOfTotients[m].Totient % listOfTotients[m].Number)
+                    {
+                        sumOfSolution = sumOfSolution + ((listOfTotients[n].Totient % listOfTotients[n].Number));
+                        Console.WriteLine(listOfTotients[n].Totient % listOfTotients[n].Number + " n=" + listOfTotients[n].Number+ " m=" + listOfTotients[n].Number);
+                    }
+                }
+            }
+            DateTime end = DateTime.Now;
+            Console.WriteLine(sumOfSolution + "!!!" + (end - start).TotalMinutes);
         }
 
-        public static long totientOf(int n)
+        
+
+        
+    }
+    class Factor
+    {
+        public long Number { get; set; }
+
+        public long Occurance { get; set; }
+
+        public Factor(long number, long occurance)
+        {
+            Number = number;
+            Occurance = occurance;
+        }       
+    }
+    class TotientPair
+    {
+        public long Number { get; set; }
+
+        public long Totient { get; set; }
+
+        public TotientPair(long number)
+        {
+            Number = number;
+            Totient = totientOf(number);
+        }
+
+        public static long totientOf(long n)
         {
             List<Factor> factors = primeFactors(n);
-            List<long> phiSegments = new List<long>();
+            List<float> phiSegments = new List<float>();
             long totient = 1;
-            foreach (Factor factor in factors )
+            foreach (Factor factor in factors)
             {
                 //count how many of those factors are in the list
 
                 //record how many and then remove the duplicates. raise factor to power of the count
-                long pk = (long)Math.Pow(factor.Number, factor.Occurance);             
-                long pBracket = 1 - (ONE / factor.Number);
-                phiSegments.Add(pk*pBracket);
+                long pk = (long)Math.Pow(factor.Number, factor.Occurance);
+                float pBracket = 1 - (1 / (float)factor.Number);
+                phiSegments.Add(pk * pBracket);
             }
             foreach (long phiSegment in phiSegments)
             {
@@ -42,12 +88,12 @@ namespace p_531
         {
             long origNumber = number;
             while (true)
-            {      
-                start:                
-                
+            {
+                start:
+
                 var factors = new List<Factor>();
 
-                if(number==0)
+                if (number == 0)
                 {
                     goto start;
                 }
@@ -55,9 +101,9 @@ namespace p_531
                 while (number % 2 == 0)
                 {
                     factorsOfTwo++;
-                    number = number / 2;                    
+                    number = number / 2;
                 }
-                if(factorsOfTwo != 0) factors.Add(new Factor(2, factorsOfTwo));
+                if (factorsOfTwo != 0) factors.Add(new Factor(2, factorsOfTwo));
 
                 for (long j = 3; j <= number; j += 2)
                 {
@@ -65,17 +111,18 @@ namespace p_531
                     while (number % j == 0)
                     {
                         factorOccurance++;
-                        number = number / j;                        
+                        number = number / j;
                     }
-                    if(factorOccurance != 0) factors.Add(new Factor(j, factorOccurance));
+                    if (factorOccurance != 0) factors.Add(new Factor(j, factorOccurance));
                 }
                 long result = 1;
                 foreach (var factor in factors)
                 {
-                    result = result* (long)Math.Pow(factor.Number,factor.Occurance);                   
+                    result = result * (long)Math.Pow(factor.Number, factor.Occurance);
                 }
-               
-                if (result == origNumber) {                     
+
+                if (result == origNumber)
+                {
                     return factors;
                 }
                 else
@@ -85,20 +132,6 @@ namespace p_531
                 }
             }
         }
-    }
-    class Factor
-    {
-        // Copy constructor.
-       
-        public Factor(long number, long occurance)
-        {
-            Number = number;
-            Occurance = occurance;
-        }
-
-        public long Number { get; set; }
-
-        public long Occurance { get; set; }
 
        
     }
